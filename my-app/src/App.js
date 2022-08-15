13ae896ae49e87231d2174a1cd76b7258edd50ce
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2022-08-15 19:45:10 +0200

    Added navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hej</h1>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Excuse me...</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Generator</Nav.Link>
+              <Nav.Link href="#link">Folder</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
