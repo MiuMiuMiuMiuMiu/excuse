2369e12b33d627ecbf5817893efed73d828e9228
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2022-08-16 11:13:23 +0200

    Added column containing h1 title

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,23 +2,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import ThemeProvider from 'react-bootstrap/ThemeProvider'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">Excuse me...</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Generator</Nav.Link>
-              <Nav.Link href="#link">Folder</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+    <ThemeProvider
+      breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+      minBreakpoint="xxs"
+    >
+      <div className="App">
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">Excuse me...</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Generator</Nav.Link>
+                <Nav.Link href="#link">Folder</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Row>
+          <Col>
+            <h1 className="text-center">Welcome to the Excuse Generator!</h1>
+          </Col>
+        </Row>
+      </div>
+    </ThemeProvider>
   );
 }
 
