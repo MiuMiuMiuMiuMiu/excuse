990eaee44f81a40ccdae471fcc4ac57aab7d1651
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2022-08-29 11:13:49 +0200

    Added listgroup to generator

--- my-app/src/pages/generator.js
+++ my-app/src/pages/generator.js
@@ -1,6 +1,16 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+import Container from 'react-bootstrap/Container';
+
 const Generator = () => {
     return (
-        "Generator page"
+        <Container className="col-4">
+            <ListGroup className="text-center" variant="flush">
+                <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+            </ListGroup>
+        </Container>
     );
 };
 
