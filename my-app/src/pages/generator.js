ab5a93b5715e5c2dfe7e14484bd82c5ea3d5255d
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2022-09-01 20:52:27 +0200

    Changed category title

--- my-app/src/pages/generator.js
+++ my-app/src/pages/generator.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 const Generator = () => {
     return (
         <Container className="pt-5 col-4 display-6">
-            <h1 className="text-center pb-4">I need an excuse for...</h1>
+            <h1 className="text-center pb-4">Categories</h1>
             <ListGroup className="text-center gap-1" variant="flush">
                 <ListGroup.Item className="category-hover">Family</ListGroup.Item>
                 <ListGroup.Item className="category-hover">Office</ListGroup.Item>
