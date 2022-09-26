90639bc25bbcc275f66535fcdcee4b8001d17edc
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2022-09-26 21:51:52 +0200

    Add component to generator
    Outputs "Hello"

--- my-app/src/pages/generator.js
+++ my-app/src/pages/generator.js
@@ -1,3 +1,4 @@
+import CallExcuse from '../components/callExcuse.js';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
 
@@ -6,11 +7,11 @@ const Generator = () => {
         <Container className="pt-5 col-4 display-6">
             <h1 className="text-center pb-4">Categories</h1>
             <ListGroup className="text-center gap-1" variant="flush">
-                <ListGroup.Item className="category-hover">Family</ListGroup.Item>
-                <ListGroup.Item className="category-hover">Office</ListGroup.Item>
-                <ListGroup.Item className="category-hover">Children</ListGroup.Item>
-                <ListGroup.Item className="category-hover">Collage</ListGroup.Item>
-                <ListGroup.Item className="category-hover">Party</ListGroup.Item>
+                <ListGroup.Item className="category-hover" onClick={CallExcuse}>Family</ListGroup.Item>
+                <ListGroup.Item className="category-hover" onClick={CallExcuse}>Office</ListGroup.Item>
+                <ListGroup.Item className="category-hover" onClick={CallExcuse}>Children</ListGroup.Item>
+                <ListGroup.Item className="category-hover" onClick={CallExcuse}>Collage</ListGroup.Item>
+                <ListGroup.Item className="category-hover" onClick={CallExcuse}>Party</ListGroup.Item>
             </ListGroup>
         </Container>
     );
