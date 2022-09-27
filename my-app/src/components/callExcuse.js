85cc1f21cc12f9095d417ef2ac41f64706dddebb
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2022-09-27 20:34:01 +0200

    Request excuse API

--- my-app/src/components/callExcuse.js
+++ my-app/src/components/callExcuse.js
@@ -1,6 +1,20 @@
+import axios from "axios";
+
+
+
+
 const CallExcuse = (props) => {
+    const axios = require('axios');
+
+    axios.get('https://excuser.herokuapp.com/v1/excuse/office')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+
     return (
-        console.log(props)
+        <div>
+        </div>
     );
 };
 
